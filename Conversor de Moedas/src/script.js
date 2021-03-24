3b7b26235b4b7bd5b299867d0ca7efba869d6030
Magda Dantas <55493802+dantasmeg1@users.noreply.github.com>
2021-03-24 19:22:16 -0300

    Conversor de Moedas
    /* 1 - Aula - CONVERSOR DE MOEDAS E VARIÁVEIS NO JAVA SCRIPT========================
    
    
    *HTML = esqueleto 
    *CSS = cor do cabelo , da pele, q de vida ao objeto
    *JS = q da movimento, musculo a ação
    
    *PROMPT = possibilidade do user interagir , digitando no popup o que o sistema perguntou  
    *ALERT = popup q sobe na tela pra informar algo 
    
    *VARIÁVEIS = onde guardamos valores, podendo ser de vários tipos , sempre camelCase.... 
    
    3 TIPOS DE ARMAZENAR AS VARIAVEIS
    *INT = numeros inteiros 
    *FLOAT = numeros decimais , separados por pontos
    *STRING = são palavras sem dentro de "aspas"
    
    
    *TOFIXED = utilizamos para fixar a qntd de casas decimais.
    toFixed(2) , estamos fizendo q apos a virgula ou ponto terá 2 casas decimais
    
    *PARSEINT = convertendo um texto para numero inteiro
    *PARSEFLOAT = convertendo um texto ou numero para decimal
    
    *OPERAÇÕOES = + - * /
    *COMENTARIOS = utilizando o // para comentários

---
+++ Conversor de Moedas/src/script.js
@@ -0,0 +1,8 @@
+var nome = prompt("Qual é o seu nome ?")
+var idadeUser = prompt("Quantos anos vc tem?") 
+alert( nome + ",sua idade atual é : " + idadeUser + " Anos")
+
+//add uma variável, q calcula +1 numero da sua idade
+var calcIdadeNum = parseInt(idadeUser)
+calcIdadeNum = calcIdadeNum + 1
+alert("Em 2022 sua idade será : " + calcIdadeNum +" Anos")
