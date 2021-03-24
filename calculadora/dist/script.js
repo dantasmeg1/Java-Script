7427616b7b0c71c34d005720b32834d6ce656e3d
Magda Dantas <55493802+dantasmeg1@users.noreply.github.com>
2021-03-24 19:17:39 -0300

    DEV IMERSAO ALURA
    /* 2 - Aula - CALCULADORA E LAÇOS CONDICIONAIS NO JAVASCRIPT ========================
    
    * DOCUMENT.WRITE = utilizamos para escrever, texto irá ser apresentado dentro da pagina, sem o popup.
    
    * CONDICIONAIS: 
      * IF = se
      * ELSE = senão
      * ELSE IF = senão se 
      
    * H2 = onde ficará o texto impresso com o document.write
    * CONCATENAÇÃO = juntar "palavra" + variável
    * COMPARAÇÃO =  igual igual ==

---
+++ calculadora/dist/script.js
@@ -0,0 +1,20 @@
+var primeiroValor = parseInt (prompt("Digite o primeiro valor: "))
+var segundoValor = parseInt(prompt("Digite o segundo valor: ")) 
+
+var operacao = prompt("Digite : 1 para realizar a Divisão, 2 para realizar a Multiplicação, 3 para realizar a Soma ou 4 para realizar a Subtração : ")
+
+if (operacao == 1){
+  var resultado = primeiroValor / segundoValor //calculo
+  document.write("<h2>" + primeiroValor + " / " + segundoValor + " = "  + resultado + "</h2>") 
+   } else if(operacao == 2){
+     var resultado = primeiroValor * segundoValor //calculo
+      document.write("<h2>" + primeiroValor + " X " + segundoValor + " = "  + resultado + "</h2>") 
+        } else if(operacao == 3){
+          var resultado = primeiroValor + segundoValor //calculo
+          document.write("<h2>" + primeiroValor + " + " + segundoValor + " = "  + resultado + "</h2>") 
+              } else if (operacao == 4){
+              var resultado = primeiroValor - segundoValor //calculo
+              document.write("<h2>" + primeiroValor + " - " + segundoValor + " = "  + resultado + "</h2>") 
+                       }else{
+                         document.write ("<h2> Opção Invalida </h2>")
+                           }
